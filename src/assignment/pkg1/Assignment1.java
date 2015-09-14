ef0f3098bcfe7673393f651f7f169be6297e8f77
Zainab Lokhandwala <c0654437@mylambton.ca>
2015-09-14 14:04:34 -0400

    Assignment 1

--- src/assignment/pkg1/Assignment1.java
+++ src/assignment/pkg1/Assignment1.java
@@ -18,5 +18,65 @@ public class Assignment1 {
         // TODO code application logic here
         System.out.println("Hello World!!");
     }
-    
+    /*********************************************************
+     * Assignment 1
+     * 
+     * 
+     * Job Title:
+Java Developer (Ottawa) 
+Tundra Technical Solution - Toronto, ON
+Link For the job:
+http://www.indeed.ca/cmp/Tundra-Technical-Solution/jobs/Java-Developer-a72a26996f875cbe?q=java
+
+Technologies needed for the job:
+
+ SDLC Methodology- Systems development life cycle methodology.
+https://en.wikipedia.org/wiki/Systems_development_life_cycle
+
+ SQL - Structured Query Language 
+https://en.wikipedia.org/wiki/SQL
+
+ETL - Extract, Transform and Load
+https://en.wikipedia.org/wiki/Extract,_transform,_load
+
+RMI - Remote Method Invocation
+https://en.wikipedia.org/wiki/Java_remote_method_invocation
+
+Swing 
+https://en.wikipedia.org/wiki/Swing_(Java)
+
+ Oracle 
+https://en.wikipedia.org/wiki/Oracle_Database
+
+
+
+Postgres 
+https://en.wikipedia.org/wiki/PostgreSQL
+
+JSON - JavaScript Object Notation
+https://en.wikipedia.org/wiki/JSON
+
+Web Services
+https://en.wikipedia.org/wiki/Web_service
+
+XML Transformation - EXtensible Markup Language
+https://en.wikipedia.org/wiki/XML_transformation_language
+
+SQL Server
+https://en.wikipedia.org/wiki/Microsoft_SQL_Server
+
+.NET
+https://en.wikipedia.org/wiki/.NET_Framework
+
+
+Salaries for this job in different cities:
+
+Toronto: C$71,942/Annual –Median
+Waterloo: C$70,664/Annual – Median
+Vancouver: C$87,630/Annual - Median
+
+
+
+
+     */
 }
